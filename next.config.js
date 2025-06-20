383399b29b102582b0b508c66cabe4da149f5679
Chinthaka Weerakkody <chinthakaflags@gmail.com>
2025-06-20 09:52:34 +0530

    uddate config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,26 @@
-import createMDX from '@next/mdx'
- 
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'sket.chin4.com',
-          },
-        ],
-        destination: '/sketchin/:path*',
+        source: "/sketchin/:path*", // This is the incoming path on chin4.com
+        destination: "https://sket.chin4.com/:path*", // This is the actual destination
+      },
+      {
+        source: "/sketchin", // Handle the root /sketchin path
+        destination: "https://sket.chin4.com/",
       },
     ];
   },
-}
- 
+};
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
- 
+});
+
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
