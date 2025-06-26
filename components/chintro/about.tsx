5bf4b4a5bb76b9e4a67486c788c8a196cb06b575
Chinthaka Weerakkody <chinthakaflags@gmail.com>
2025-06-26 17:56:13 +0530

    updated content

--- components/chintro/about.tsx
+++ components/chintro/about.tsx
@@ -6,16 +6,17 @@ export default function About() {
   return (
     <section className="mb-16">
       <div className="text-xl sm:text-xl text-foreground text-justify space-y-4">
-        <p>
-          The most challenging question in an interview isn&apos;t about
-          algorithms it&apos;s &quot;Tell me about yourself.&quot;
+        {/* <p>
+          The most challenging question in an technical interview isn&apos;t about
+          algorithms 
         </p>
+        <p>it&apos;s &quot;Tell me about yourself.&quot;</p>
         <p>I don&apos;t know how to answer both.</p>
         <p>
           I&apos;m constantly trying to figure it out, and most of the time,
           there&apos;s no definitive answer. So I&apos;ll fall back to the most
           standard version
-        </p>
+        </p> */}
         <p>
           My name is <span className="font-bold">Chinthaka</span>, it means
           “philosopher” or “thinker” in my language,{" "}
@@ -38,9 +39,22 @@ export default function About() {
           paid for it. That still gives me some bragging rights... at least
           until the robots take over.
         </p>
+        <p>I write, too </p>{" "}
         <p>
-          But before diving into development, I was a mechanical engineer.
-          It&apos;s been a fun journey moving into dev.
+          sometimes about code, sometimes about hallucinations, reasonably fun
+          comedies, dreams, political drama, or the strange overlap between all
+          of them.
+        </p>
+        <p>
+          But if you think about it, even coding is writing, writing that only
+          machines can understand.
+        </p>
+        <p>Machines are easier to please</p>
+        <p>
+          I have also worked with machines that don&apos;t run on zeros and ones
+        </p>
+        <p>
+          Curous to know ?
           <Link
             href="/sketchin/from-nuts-and-bolts-to-ones-zeros"
             target="_new"
